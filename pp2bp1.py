11605d8158851c4de2c45019bb12497846501adb
Jaeman Lee <jaemann.lee@gmail.com>
2022-05-06 06:31:15 +0900

    Update pp2bp1.py

--- pp2bp1.py
+++ pp2bp1.py
@@ -42,14 +42,12 @@ def check_gene_pathogenic_mechanism(
         <병의 원인 판단 기준>
         - Missense: 아래 기준 모두 포함
             1. pathogenic missense variant 2개 이상
-            2. 전체 variant 중 50% 이상이 pathogenic.
-            3. pathogenic variant 중, pathogenic missense가 50% 이상.
+            2. pathogenic variant 중, pathogenic missense가 50% 이상.
 
         - Null variant: 아래 기준 모두 포함.
-        {nonsense, frameshift variant, splice donor variant, splice acceptor variant}
+        {nonsense, frameshift variant, splice donor variant, splice acceptor variant, initiatior codon variant}
             1. pathogenic null variant 2개 이상
-            2. 전체 variant 중 50% 이상이 pathogenic.
-            3. pathogenic variant 중, pathogenic null variant가 50% 이상.
+            2. pathogenic variant 중, pathogenic null variant가 50% 이상.
 
     Args:
         gene_variant_dic (dict): gene_symbol을 기준으로 정리한 ClinVar data dictionary
@@ -61,7 +59,6 @@ def check_gene_pathogenic_mechanism(
 
     pathogenic_set = {"Pathogenic", "Likely pathogenic"}
 
-    total_variant_count = len(gene_variant_dic)
     pathogenic_missense_count = 0
     pathogenic_null_count = 0
     pathogenic_count = 0
@@ -77,27 +74,24 @@ def check_gene_pathogenic_mechanism(
             + var_cons_dic.get("frameshift variant", 0)
             + var_cons_dic.get("splice donor variant", 0)
             + var_cons_dic.get("splice acceptor variant", 0)
+            + var_cons_dic.get("initiatior codon variant", 0)
         )
         if var_patho in pathogenic_set:  # pathogenic variant
             pathogenic_count += 1
 
-            if missense_count / sum(var_cons_dic.values()) > 0.5:
+            if (missense_count / sum(var_cons_dic.values())) > 0.5:
                 pathogenic_missense_count += 1
-            elif null_count / sum(var_cons_dic.values()) > 0.5:
+            elif (null_count / sum(var_cons_dic.values())) > 0.5:
                 pathogenic_null_count += 1
 
     # 기준에 따라 병의 원인 메커니즘 부여.
-    if (
-        pathogenic_missense_count >= 2
-        and pathogenic_count / total_variant_count > 0.5
-        and pathogenic_missense_count / pathogenic_count > 0.5
-    ):
+    if (pathogenic_missense_count >= 2) and (
+        pathogenic_missense_count / pathogenic_count
+    ) > 0.5:
         return "missense variant"
-    elif (
-        pathogenic_null_count >= 2
-        and pathogenic_count / total_variant_count > 0.5
-        and pathogenic_null_count / pathogenic_count > 0.5
-    ):
+    elif (pathogenic_null_count >= 2) and (
+        pathogenic_null_count / pathogenic_count
+    ) > 0.5:
         return "null variant"
 
     return ""
@@ -145,8 +139,8 @@ def execute(
 
         pp2: Missense 변이가 해당 질병의 일반적인 원인이며, 주로 pathogenic 한 변이가 보고된 유전자에서의
         missense 변이
-        bp1: Truncating variant (null variant)가 질병의 주된 원인이라고 보고된 유전자에서의
-        missense 변이
+        bp1: Truncating variant (null variant)가 질병의 유일하게 알려진 원인이라고 보고된 유전자에서의
+        missense 변이. pathogenic_null_count / pathogenic count == 1?
 
         본 모듈에서는 1가지의 데이터베이스가 사용되었다.
         - ClinVar: 동일한 유전자를 기준으로 하여 변이들을 정리하였다(RCV).
@@ -158,8 +152,12 @@ def execute(
         }
 
         본 모듈의 작동방식은 다음과 같다.
-        먼저, 각 variant에 대하여 missense variant인지 아닌지 조사한다(is_missense_var()). 만약 해당 변이의 전사체가 missense variant라면, 해당 변이의 gene_symbol과 일치하는 ClinVar 데이터들을 조사하여, 해당 유전자에서
-        발생하는 질병의 원인이 missense variant인지, null variant인지 계산한다(check_gene_pathogenic_mechanism()). 만약, 해당 유전자에서 주로 pathogenic 한 변이가 보고되고, missense 변이가 일반적인 원인이면 pp2를 부여하며, null variant가 질병의 주된 원인이라면 bp1을 부여한다(assign_pp2bp1_rule()).
+        먼저, 각 variant에 대하여 missense variant인지 아닌지 조사한다(is_missense_var()).
+        만약 해당 변이의 전사체가 missense variant라면, 해당 변이의 gene_symbol과 일치하는 ClinVar 데이터들을 조사하여,
+        해당 유전자에서 발생하는 질병의 원인이 missense variant인지, null variant인지 계산한다
+        (check_gene_pathogenic_mechanism()). 만약, 해당 유전자에서 주로 pathogenic 한 변이가 보고되고,
+        missense 변이가 일반적인 원인이면 pp2를 부여하며, null variant가 질병의 주된 원인이라면 bp1을 부여한다
+        (assign_pp2bp1_rule()).
 
     Args:
         proband_var_df (object): Class VariantDF <- VEP file
